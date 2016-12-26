a39275a4aa293416adc44b259ef6aa85c543cbba
wangda <781654805@qq.com>
2016-12-26 16:16:44 +0800

    (123)

--- regexp.js
+++ regexp.js
@@ -6,6 +6,23 @@
  *   \n换行符 \f换页符 \r回车符 \t制表符  \v垂直制表符
  * 4.量词 +至少一个字符串 *零个或多个  ？零个或者一个   {x}x个 {x，y}x至y个  ^n n$以n开头，以n结尾  ?=n匹配其后紧接字符串n的字符串  ?!n匹配没有紧接字符串n的字符串
  */
-//RegExp对象方法 compile exec test
+//RegExp对象方法 exec test
 
-//compile：
+//exec: regexp.exec(str)   //返回检索的结果或者为null
+var str = "hello world";
+var patt = new RegExp(/w/,"g");
+var result = patt.exec(str); //w
+var lastindex = patt.lastIndex; //7
+
+//test: regexp.text(str)  //返回true或者false
+var str1 = "Hello world";
+var patt1 = new RegExp(/hello/,"i");
+var result1 = patt1.test(str1); //true
+
+//search str.search(regexp) //返回index（第一个），如果找不到就是-1
+var str2 = "Hello world";
+var result2 = str2.search(/w/g); //6
+
+//match str.match(regexp) //返回检索到的，以array形式，没有的话就是空
+var str3 = "H1ello w1orld";
+var result3 = str3.match(/\w\d/g); //[H1,w1]
